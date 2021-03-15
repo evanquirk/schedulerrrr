2f81d6803804e1687d8dc7961edb1dc6e2fb3e2c
Evan Quirk <evanquirk>
2021-03-15 16:24:29 -0700

    add DayListItem components to page with styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,11 @@
 import React from "react";
 
 import "components/Button.scss";
-import { action } from "@storybook/addon-actions/dist/preview";
+import classNames from "classnames";
 
 export default function Button(props) {
-  let buttonClass = "button";
-
-  if (props.confirm) {
-    buttonClass += " button--confirm";
-  }
-
-  if (props.danger) {
-    buttonClass += " button--danger";
-  }
+  
+  let buttonClass = classNames('button', {'button--confirm': props.confirm}, {'button--danger': props.danger});
 
   return (
     <button
