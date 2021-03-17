3dd56ddfde27c52d11ad0d340315ed86072329b7
Evan Quirk <evanquirk>
2021-03-17 15:42:39 -0700

    feat: add axios and day handling through axios api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
+import axios from 'axios';
 import DayList from "./DayList";
 import "components/Application.scss";
-import Appointment from "components/Appointment/index.js";
+import Appointment from "components/Appointment";
+
 
 const appointments = [
   {
@@ -56,27 +58,18 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-  
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get('/api/days')
+    .then(response => {setDays([...response.data])
+  });} ,[])
+  //   return () => {
+  //     cleanup
+  //   }
+  // }, [input])
   return (
     <main className="layout">
       <section className="sidebar">
@@ -103,7 +96,7 @@ export default function Application(props) {
       <section className="schedule">
     {appointments.map((appointment) => 
           <Appointment
-            id={appointment.id}
+            key={appointment.id}
               {...appointment}
             />
         )}
