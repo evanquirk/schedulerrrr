6739cdec412a634bff46db2f155d40c14c2b9c4f
Evan Quirk <evanquirk>
2021-03-24 18:02:14 -0700

    fix: add edge case for interviewer missing in form test file

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -47,9 +47,9 @@ describe("Form", () => {
 
   //===========================================================
 
-  xit("can successfully save after trying to submit an empty student name", () => {
+  it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
+    const { getByText, getByPlaceholderText, queryByText , getByAltText } = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
 
@@ -62,12 +62,14 @@ describe("Form", () => {
       target: { value: "Lydia Miller-Jones" }
     });
 
+    fireEvent.click(getByAltText("Sylvia Palmer"))
+
     fireEvent.click(getByText("Save"));
 
     expect(queryByText(/student name cannot be blank!/i)).toBeNull();
 
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
 
   //===========================================================
